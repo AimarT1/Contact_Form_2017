ef6eee2fd28a442119ea3421534e06c0cb4df60f
AimarT <aimartamm@hotmail.com>
2018-12-19 21:40:44 +0200

    Contact Form 2017

---
+++ php/sendMessage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 321990
+ * Date: 19.12.2017
+ * Time: 12:09
+ */
+?>
+
+<html>
+<head>
+<title>Contact Form using JavaScript </title>
+		<!-- include css file here-->
+<link rel="stylesheet" href="../css/sendMessage.css"/>
+   
+</head>
+<body>
+
+<div id="main">
+<left>
+<h1>Company Name</h1>
+</left>
+
+    <div id="form_sample">
+	
+</div>
+   <!-- include JavaScript file here -->
+<script src="../js/sendMessage.js"></script>
+
+</body>
+</html>
